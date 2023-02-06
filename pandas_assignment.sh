e977cba091c1d5fccd3357dc2b3ac0dab0132801
CiCi-Bio <122321202+CiCi-Bio@users.noreply.github.com>
2023-02-05 20:59:52 -0500

    Add files via upload

---
+++ pandas_assignment.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+name="Candice's panda's assignment"
+echo $name 
+sed '/zoo/d' pandas.txt>> pandas_no_zoo.txt
+grep "sex" pandas.txt | awk '{print $1,$4,$2}' pandas.txt>> pandas_name_age_sex.txt
+sort pandas.txt>> pandas_alpha.txt
+sort -k5 pandas.txt>> pandas_zooalpha.txt
+sed 's/female/F' | sed 's/male/M/' pandas.txt>> pandas_MF.txt
+grep -c "female" pandas.txt | grep -cv "October"
+nl pandas.txt>> pandas_line_numbers.txt
+sed -n '1p;6p;17p' pandas_line_numbers.txt>> pandas_print_numbers.txt
+grep -in "X" pandas.txt>> pandas_nox.txt
+sort -k5 pandas.txt | awk '{print $1,$5}' | uniq -u pandas.txt>> pandas_location.txt
+sort -nk4 pandas.txt>> pandas_age.txt
+awk '$4 <=10 {print $1,$4}' pandas_age.txt>> pandas_age_range.txt
