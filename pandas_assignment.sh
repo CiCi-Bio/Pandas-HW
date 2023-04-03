57b9886d200c2d7ad230109621dfed80aebac5d4
CiCi-Bio <122321202+CiCi-Bio@users.noreply.github.com>
2023-04-03 11:54:00 -0400

    Update pandas_assignment.sh

--- pandas_assignment.sh
+++ pandas_assignment.sh
@@ -5,7 +5,7 @@ sed 's/Zoo//g' pandas.txt>> pandas_no_zoo.txt
 grep "sex" pandas.txt | awk '{print $1,$4,$2}' pandas.txt>> pandas_name_age_sex.txt
 sort pandas.txt>> pandas_alpha.txt
 sort -k5 pandas.txt>> pandas_zooalpha.txt
-sed 's/"female"/F' pandas.txt | sed 's/male/M/'>> pandas_MandF.txt
+sed "s/female"/F/" pandas.txt | sed "s/male/M/" >> pandas_MandF.txt
 grep -c "female" pandas.txt | grep -cv "October"
 nl pandas.txt>> pandas_line_numbers.txt
 sed -n '1p;6p;17p' pandas_line_numbers.txt>> pandas_print_numbers.txt
